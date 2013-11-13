8411ce0ff32e967577ca611c454deb21b4e32707
Michele Bertasi <michele.bertasi@gmail.com>
2013-11-13 17:59:58 +0100

    [docs] Cleaned up example

--- neo/libs/ffmpeg/doc/examples/decoding.c
+++ neo/libs/ffmpeg/doc/examples/decoding.c
@@ -43,60 +43,6 @@
 
 #define INBUF_SIZE 4096
 #define AUDIO_INBUF_SIZE 20480
-#define AUDIO_REFILL_THRESH 4096
-
-/* check that a given sample format is supported by the encoder */
-static int check_sample_fmt(AVCodec *codec, enum AVSampleFormat sample_fmt)
-{
-    const enum AVSampleFormat *p = codec->sample_fmts;
-
-    while (*p != AV_SAMPLE_FMT_NONE) {
-        if (*p == sample_fmt)
-            return 1;
-        p++;
-    }
-    return 0;
-}
-
-/* just pick the highest supported samplerate */
-static int select_sample_rate(AVCodec *codec)
-{
-    const int *p;
-    int best_samplerate = 0;
-
-    if (!codec->supported_samplerates)
-        return 44100;
-
-    p = codec->supported_samplerates;
-    while (*p) {
-        best_samplerate = FFMAX(*p, best_samplerate);
-        p++;
-    }
-    return best_samplerate;
-}
-
-/* select layout with the highest channel count */
-static int select_channel_layout(AVCodec *codec)
-{
-    const uint64_t *p;
-    uint64_t best_ch_layout = 0;
-    int best_nb_channels   = 0;
-
-    if (!codec->channel_layouts)
-        return AV_CH_LAYOUT_STEREO;
-
-    p = codec->channel_layouts;
-    while (*p) {
-        int nb_channels = av_get_channel_layout_nb_channels(*p);
-
-        if (nb_channels > best_nb_channels) {
-            best_ch_layout    = *p;
-            best_nb_channels = nb_channels;
-        }
-        p++;
-    }
-    return best_ch_layout;
-}
 
 
 /*
@@ -144,129 +90,28 @@ static int decode_write_frame(const char *outfilename, AVCodecContext *avctx,
     return 0;
 }
 
-#if 0
-
-typedef struct MyAVPacketList {
-    AVPacket pkt;
-    struct MyAVPacketList *next;
-    int serial;
-} MyAVPacketList;
-
-typedef struct PacketQueue {
-    MyAVPacketList *first_pkt, *last_pkt;
-    int nb_packets;
-    int size;
-    int abort_request;
-    int serial;
-} PacketQueue;
-
-typedef struct VideoState
-{
-    PacketQueue videoq;
-    AVStream *video_st;
-    double frame_timer;
-    double frame_last_pts;
-    double frame_last_duration;
-    double frame_last_dropped_pts;
-    int video_finished;
-} VideoState;
-
-
-static int get_video_frame(VideoState *is, AVFrame *frame, AVPacket *pkt, int *serial)
-{
-    int got_picture;
-
-    if (packet_queue_get(&is->videoq, pkt, 1, serial) < 0)
-        return -1;
-
-    if (pkt->data == flush_pkt.data) {
-        avcodec_flush_buffers(is->video_st->codec);
-        is->video_current_pos = -1;
-        is->frame_last_pts = AV_NOPTS_VALUE;
-        is->frame_last_duration = 0;
-        is->frame_timer = (double)av_gettime() / 1000000.0;
-        is->frame_last_dropped_pts = AV_NOPTS_VALUE;
-        return 0;
-    }
-
-    if(avcodec_decode_video2(is->video_st->codec, frame, &got_picture, pkt) < 0)
-        return 0;
-
-    if (!got_picture && !pkt->data)
-        is->video_finished = *serial;
-
-    if (got_picture) {
-        int ret = 1;
-        double dpts = NAN;
-
-        if (decoder_reorder_pts == -1) {
-            frame->pts = av_frame_get_best_effort_timestamp(frame);
-        } else if (decoder_reorder_pts) {
-            frame->pts = frame->pkt_pts;
-        } else {
-            frame->pts = frame->pkt_dts;
-        }
-
-        if (frame->pts != AV_NOPTS_VALUE)
-            dpts = av_q2d(is->video_st->time_base) * frame->pts;
-
-        frame->sample_aspect_ratio = av_guess_sample_aspect_ratio(is->ic, is->video_st, frame);
-
-#if 0
-        if (framedrop>0 || (framedrop && get_master_sync_type(is) != AV_SYNC_VIDEO_MASTER)) {
-            if (is->frame_last_pts != AV_NOPTS_VALUE && frame->pts != AV_NOPTS_VALUE) {
-                double clockdiff = get_clock(&is->vidclk) - get_master_clock(is);
-                double ptsdiff = dpts - is->frame_last_pts;
-                if (!isnan(clockdiff) && fabs(clockdiff) < AV_NOSYNC_THRESHOLD &&
-                    !isnan(ptsdiff) && ptsdiff > 0 && ptsdiff < AV_NOSYNC_THRESHOLD &&
-                    clockdiff + ptsdiff - is->frame_last_filter_delay < 0 &&
-                    is->videoq.nb_packets) {
-                    is->frame_last_dropped_pos = av_frame_get_pkt_pos(frame);
-                    is->frame_last_dropped_pts = dpts;
-                    is->frame_last_dropped_serial = *serial;
-                    is->frame_drops_early++;
-                    av_frame_unref(frame);
-                    ret = 0;
-                }
-            }
-        }
-#endif
-        return ret;
-    }
-    return 0;
-}
-
-#endif
-
 static int my_video_decode(const char* outfilename, const char* filename)
 {
     int err, ret, frame_count;
     AVFormatContext *ic = NULL;
     AVInputFormat *iformat = NULL;
-    AVDictionary *format_opts = NULL, *codec_opts = NULL;
-    AVDictionary **opts = NULL;
     AVCodec *codec;
     AVCodecContext *avctx = NULL;
-    FILE* f;
     AVFrame *frame = av_frame_alloc();
-    uint8_t inbuf[INBUF_SIZE + FF_INPUT_BUFFER_PADDING_SIZE];
     AVPacket avpkt;
     AVStream* video_st;
 
     av_init_packet(&avpkt);
-    /* set end of buffer to 0 (this ensures that no overreading happens for damaged mpeg streams) */
-    memset(inbuf + INBUF_SIZE, 0, FF_INPUT_BUFFER_PADDING_SIZE);
-
 
     // read_loop
 
     ic = avformat_alloc_context();
-    err = avformat_open_input(&ic, filename, iformat, &format_opts);
+    err = avformat_open_input(&ic, filename, iformat, NULL);
     if (err < 0) {
         ret = -1;
         goto fail;
     }
-    err = avformat_find_stream_info(ic, opts);
+    err = avformat_find_stream_info(ic, NULL);
     if (err < 0) {
         ret = -1;
         goto fail;
@@ -344,8 +189,6 @@ static int my_video_decode(const char* outfilename, const char* filename)
     avpkt.size = 0;
     decode_write_frame(outfilename, avctx, frame, &frame_count, &avpkt, 1);
 
-    fclose(f);
-
     avcodec_close(avctx);
     av_free(avctx);
     avcodec_free_frame(&frame);
@@ -355,126 +198,16 @@ fail:
     return ret;
 }
 
-static void video_decode_example(const char *outfilename, const char *filename)
-{
-    AVFormatContext *ic = NULL;
-    AVInputFormat *iformat = NULL;
-    AVDictionary *format_opts = NULL;
-    AVCodec *codec;
-    AVCodecContext *c= NULL;
-    int frame_count, err;
-    FILE *f;
-    AVFrame *frame;
-    uint8_t inbuf[INBUF_SIZE + FF_INPUT_BUFFER_PADDING_SIZE];
-    AVPacket avpkt;
-
-
-    av_init_packet(&avpkt);
-
-    /* set end of buffer to 0 (this ensures that no overreading happens for damaged mpeg streams) */
-    memset(inbuf + INBUF_SIZE, 0, FF_INPUT_BUFFER_PADDING_SIZE);
-
-    printf("Decode video file %s to %s\n", filename, outfilename);
-
-    err = avformat_open_input(&ic, filename, iformat, &format_opts);
-
-//    /* find the mpeg1 video decoder */
-//    codec = avcodec_find_decoder(AV_CODEC_ID_BINKVIDEO);
-//    if (!codec) {
-//        fprintf(stderr, "Codec not found\n");
-//        exit(1);
-//    }
-
-    c = avcodec_alloc_context3(codec);
-    if (!c) {
-        fprintf(stderr, "Could not allocate video codec context\n");
-        exit(1);
-    }
-
-    if(codec->capabilities&CODEC_CAP_TRUNCATED)
-        c->flags|= CODEC_FLAG_TRUNCATED; /* we do not send complete frames */
-
-    /* For some codecs, such as msmpeg4 and mpeg4, width and height
-       MUST be initialized there because this information is not
-       available in the bitstream. */
-
-    /* open it */
-    if (avcodec_open2(c, codec, NULL) < 0) {
-        fprintf(stderr, "Could not open codec\n");
-        exit(1);
-    }
-
-    f = fopen(filename, "rb");
-    if (!f) {
-        fprintf(stderr, "Could not open %s\n", filename);
-        exit(1);
-    }
-
-    frame = avcodec_alloc_frame();
-    if (!frame) {
-        fprintf(stderr, "Could not allocate video frame\n");
-        exit(1);
-    }
-
-    frame_count = 0;
-    for(;;) {
-        avpkt.size = fread(inbuf, 1, INBUF_SIZE, f);
-        if (avpkt.size == 0)
-            break;
-
-        /* NOTE1: some codecs are stream based (mpegvideo, mpegaudio)
-           and this is the only method to use them because you cannot
-           know the compressed data size before analysing it.
-
-           BUT some other codecs (msmpeg4, mpeg4) are inherently frame
-           based, so you must call them with all the data for one
-           frame exactly. You must also initialize 'width' and
-           'height' before initializing them. */
-
-        /* NOTE2: some codecs allow the raw parameters (frame size,
-           sample rate) to be changed at any frame. We handle this, so
-           you should also take care of it */
-
-        /* here, we use a stream based decoder (mpeg1video), so we
-           feed decoder and see if it could decode a frame */
-        avpkt.data = inbuf;
-        while (avpkt.size > 0)
-            if (decode_write_frame(outfilename, c, frame, &frame_count, &avpkt, 0) < 0)
-                exit(1);
-    }
-
-    /* some codecs, such as MPEG, transmit the I and P frame with a
-       latency of one frame. You must do the following to have a
-       chance to get the last frame of the video */
-    avpkt.data = NULL;
-    avpkt.size = 0;
-    decode_write_frame(outfilename, c, frame, &frame_count, &avpkt, 1);
-
-    fclose(f);
-
-    avcodec_close(c);
-    av_free(c);
-    avcodec_free_frame(&frame);
-    printf("\n");
-}
-
 
 int main(int argc, char **argv)
 {
     const char *input;
 
     /* register all the codecs */
-    avcodec_register_all();
     av_register_all();
 
     if (argc < 2) {
-        printf("usage: %s output_type\n"
-               "API example program to decode/encode a media stream with libavcodec.\n"
-               "This program generates a synthetic stream and encodes it to a file\n"
-               "named test.h264, test.mp2 or test.mpg depending on output_type.\n"
-               "The encoded stream is then decoded and written to a raw data output.\n"
-               "output_type must be choosen between 'h264', 'mp2', 'mpg'.\n",
-               argv[0]);
+        printf("usage: %s input.bik\n", argv[0]);
         return 1;
     }
     input = argv[1];
